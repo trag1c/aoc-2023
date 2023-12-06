1ccac8b7519322635494cde5e367642694547f0e
trag1c <trag1cdev@yahoo.com>
2023-12-06 13:10:53 +0100

    Day 6

--- src/solutions.rs
+++ src/solutions.rs
@@ -3,12 +3,14 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 pub use day1::DAY1;
 pub use day2::DAY2;
 pub use day3::DAY3;
 pub use day4::DAY4;
 pub use day5::DAY5;
+pub use day6::DAY6;
 
 mod solution;
 pub use solution::Solution;
---
+++ src/solutions/day6.rs
@@ -0,0 +1,36 @@
+use super::Solution;
+
+fn part1(input: &str) {
+    fn get_nums(line: &str) -> Vec<i32> {
+        line.split_once(':')
+            .unwrap()
+            .1
+            .split_whitespace()
+            .map(&str::parse::<i32>)
+            .map(Result::unwrap)
+            .collect::<Vec<i32>>()
+    }
+    let (time_str, distance_str) = input.split_once('\n').unwrap();
+    let (times, distances) = (get_nums(time_str), get_nums(distance_str));
+
+    let mut product = 1;
+    for (time, distance) in times.iter().zip(distances) {
+        let f = move |x: i32| (time - x) * x;
+        product *= (1..*time)
+            .map(|x| f(x) > distance)
+            .map(u32::from)
+            .sum::<u32>();
+    }
+    println!("{product}");
+}
+
+fn part2(input: &str) {
+    fn get_num(line: &str) -> i64 {
+        line.split_once(':').unwrap().1.replace(' ', "").parse::<i64>().unwrap()
+    }
+    let (time_str, distance_str) = input.split_once('\n').unwrap();
+    let (time, distance) = (get_num(time_str), get_num(distance_str));
+    println!("{}", (1..time).map(move |x| (time - x) * x > distance).map(u32::from).sum::<u32>());
+}
+
+pub const DAY6: Solution = Solution { part1, part2 };
